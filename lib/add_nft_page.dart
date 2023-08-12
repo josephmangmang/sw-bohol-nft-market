51d4fc7dfd9ec4cdc3be61cb18ec5cce50114cc0
josephmangmang <joseph@blendit.eu>
2023-08-12 10:58:15 +0800

    add back button

--- lib/add_nft_page.dart
+++ lib/add_nft_page.dart
@@ -7,66 +7,84 @@ class AddNftPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-      child: Container(
-        padding: const EdgeInsets.symmetric(vertical: 16),
-        child: Column(
-          children: [
-            Expanded(
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    const Center(
-                      child: Text(
-                        'Add NFT',
-                        style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    const SizedBox(height: 48),
-                    const Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 16),
-                      child: TextField(
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'NFT Name',
+      child: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Stack(
+                      children: [
+                        Align(
+                          alignment: AlignmentDirectional.centerStart,
+                          child: IconButton.filledTonal(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              icon: const Icon(
+                                Icons.arrow_back,
+                                color: Colors.white,
+                              )),
                         ),
-                      ),
-                    ),
-                    // Price
-                    const SizedBox(height: 16),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 16),
-                      child: TextField(
-                        keyboardType: TextInputType.number,
-                        decoration: InputDecoration(
-                          border: const OutlineInputBorder(),
-                          labelText: 'Price',
-                          prefixIcon: Image.asset(
-                            'assets/images/Ethereum.png',
-                            width: 20,
-                            height: 20,
+                        const Center(
+                          child: Text(
+                            'NFT Marketplace',
+                            style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
                           ),
                         ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 48),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'NFT Name',
                       ),
                     ),
-                    Container(
-                      height: 500,
-                      margin: const EdgeInsets.all(16),
-                      color: Colors.white.withOpacity(0.2),
-                      child: const Center(child: Text('Select Image')),
-                    )
-                  ],
-                ),
+                  ),
+                  // Price
+                  const SizedBox(height: 16),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: TextField(
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        border: const OutlineInputBorder(),
+                        labelText: 'Price',
+                        prefixIcon: Image.asset(
+                          'assets/images/Ethereum.png',
+                          width: 20,
+                          height: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 500,
+                    margin: const EdgeInsets.all(16),
+                    color: Colors.white.withOpacity(0.2),
+                    child: const Center(child: Text('Select Image')),
+                  )
+                ],
               ),
             ),
-            const SizedBox(
-              height: 16,
-            ),
-            FilledButton.tonal(
-              onPressed: () {},
-              child: const Text('Add NFT'),
-            )
-          ],
-        ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          FilledButton.tonal(
+            onPressed: () {},
+            child: const Text('Add NFT'),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+        ],
       ),
     ));
   }
